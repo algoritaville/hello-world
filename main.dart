f4497589bd9df3135a0f803cb17799c643184984
Matt Kotys <matthew.kotys@demirconet.local>
2018-07-24 12:25:57 -0500

    Created a Person class and a method that calculates age based on birthday.

--- main.dart
+++ main.dart
@@ -1,9 +1,28 @@
 // Test dart file.
-String getName() {
-  return "Hello I am Matt";
-}
 
 main() {
-  print("This is a test." + "\n" + getName());
+  var jim_carrey = new Person("Jim", "Carrey", new DateTime(1962, 1, 17));
+  print(jim_carrey.getName());
+
+  var kristen_wiig = new Person("Kristen", "Wiig", new DateTime(1973, 8, 22));
+  print(kristen_wiig.getName());
+
+  var bernadette_peters = new Person("Bernadette", "Peters", new DateTime(1948, 2, 28));
+  print(bernadette_peters.getName());
+
+  var mel_brooks = new Person("Mel", "Brooks", new DateTime(1928, 6, 28));
+  print(mel_brooks.getName());
 }
 
+class Person {
+  String firstName, lastName;
+  DateTime birthday;
+  double age;
+
+  Person(this.firstName, this.lastName, this.birthday) {
+    var now = new DateTime.now();
+    this.age = now.difference(this.birthday).inSeconds / 3600 / 24 / 365;
+  }
+
+  String getName() => "Hello, I am ${this.firstName} ${this.lastName}, and I am ${this.age.round()} years old.";
+}
